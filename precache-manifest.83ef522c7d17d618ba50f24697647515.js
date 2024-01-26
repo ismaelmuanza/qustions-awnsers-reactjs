26afdb2ade364a0f2caf77eb769508e02e3e564c
Ismael Muanza <ismaelmuanza99@gmail.com>
2024-01-26 22:09:53 +0100

    first commit

---
+++ precache-manifest.83ef522c7d17d618ba50f24697647515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef82ad9f864b966f3e3c027aa177a57f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7aad8a188c43d3bf329",
+    "url": "/static/css/main.3e9056ae.chunk.css"
+  },
+  {
+    "revision": "e6426d6cee39231f78fc",
+    "url": "/static/js/2.9a6e14ae.chunk.js"
+  },
+  {
+    "revision": "acd877ff1ffd03428aaee5748d0729d7",
+    "url": "/static/js/2.9a6e14ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7aad8a188c43d3bf329",
+    "url": "/static/js/main.7fab2c76.chunk.js"
+  },
+  {
+    "revision": "2048b3f90d69c5897180",
+    "url": "/static/js/runtime-main.aaba5821.js"
+  }
+]);
